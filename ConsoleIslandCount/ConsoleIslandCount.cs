3b5add40cce3ac6109566e3438ddc8b16218350b
ppayne <ppayne@sureid.com>
2017-09-07 13:30:26 -0700

    Working island counter pre testing

--- ConsoleIslandCount/ConsoleIslandCount.cs
+++ ConsoleIslandCount/ConsoleIslandCount.cs
@@ -51,30 +51,84 @@ X---------
 
         public static int IslandCount(bool [,] grid)
         {
-            var gridSize = grid.Length;
+            var gridSize = (int)Math.Sqrt(grid.Length);
             var islandCount = 0;
             var visitedMap = new bool[gridSize, gridSize];
 
-            for (int row = 0; row < gridSize; row++)
+            for (var row = 0; row < gridSize; row++)
             {
-                for (int col = 0; col < gridSize; col++)
+                for (var col = 0; col < gridSize; col++)
                 {
                     if (visitedMap[row,col])
                     {
                         continue;
                     }
 
-                    islandCount += IslandMapper(row, col, grid, visitedMap);
+                    if (IslandMapper(new Coordinate(row,col), grid, visitedMap, gridSize))
+                    {
+                        ++islandCount;
+                    }
                 }
             }
             return islandCount;
         }
 
-        private static int IslandMapper(int row, int col, bool[,] grid, bool[,] map)
+        private static bool IslandMapper(Coordinate position, bool[,] grid, bool[,] map, int size)
         {
-            int landFound = 0;
+            var landHo = grid[position.Row, position.Column];
+            map[position.Row, position.Column] = true;
+            if (!landHo)
+            {
+                return landHo;
+            }
 
-            return landFound;
+            var parimeter = DefineParimeter(position, size);
+            foreach (var coordinate in parimeter.Where(p => !map[p.Row, p.Column]))
+            {
+                IslandMapper(coordinate, grid, map, size);
+            }
+
+            return landHo;
         }
+
+        public static List<Coordinate> DefineParimeter(Coordinate center, int size)
+        {
+            var coordinates = new List<Coordinate>();
+
+            for (var xPos = center.Row - 1; xPos < (center.Row + 2); xPos++)
+            {
+                //bounds check
+                if (xPos < 0 || xPos >= size)
+                {
+                    continue;
+                }
+
+                for (var yPos = center.Column -1; yPos < (center.Column + 2); yPos++)
+                {
+                    //bounds and center check 
+                    if (yPos < 0 || yPos >= size || (xPos == center.Row && yPos == center.Column ))
+                    {
+                        continue;
+                    }
+
+                    coordinates.Add(new Coordinate(xPos, yPos));
+                }
+            }
+
+            return coordinates;
+        }
+    }
+    
+
+    public class Coordinate
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Coordinate(int row, int col)
+        {
+            Row = row;
+            Column = col;
+        }   
     }
 }
